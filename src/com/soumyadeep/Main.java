72dfe77b448a34b3cb7e2883aa9dc0feadb56ec9
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-09-07 00:01:38 +0530

    [2.0.0] Encapsulation Done. OOP

--- src/com/soumyadeep/Main.java
+++ src/com/soumyadeep/Main.java
@@ -3,14 +3,12 @@ package com.soumyadeep;
 public class Main {
 
     public static void main(String[] args) {
-        int baseSalary = 50_000;
-        int extraHours = 10;
-        int hourlyRate = 50;
 
-        System.out.println(calculateWage(baseSalary, extraHours, hourlyRate));
-    }
+        var employee = new Employee();
 
-    public static int calculateWage(int baseSalary, int extraHours, int hourlyRate) {
-        return baseSalary + (extraHours * hourlyRate);
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 50;
+
+        System.out.println(employee.calculateWage(10));
     }
 }
